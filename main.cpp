0382dfeb9940071fc13a3c48608a0c9e20e6d6cf
luciana1faur <luciana_faur@yahoo.com>
2020-09-19 06:33:19 +0000

    IntoducereDate

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <fstream>
 
-int main() {
-  std::cout << "Hello World!\n";
+using namespace std;
+
+int main()
+{
+   fstream fd;
+   int s, c1, c2;
+   char mesaj;
+   fd.open( "fdate.txt", ios::app);
+   mesaj = 'd';
+   while (tolower(mesaj) == 'd')
+   {
+      cout << "Introduceti setul (sectie cand_1 cand_2): ";
+      cin >> s >> c1 >> c2;
+      fd << s << '\t' << c1 << '\t' << c2 << endl;
+      cin.ignore();
+      cout << "Mai introduceti date? (d/n) ";
+      cin >> mesaj;
+   }
+   fd.close();
+   return 0;
 }
